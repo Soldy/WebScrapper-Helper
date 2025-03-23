b5813e00054b2ee1b1230f6dae76793206910c3d
Soldy <4786022+Soldy@users.noreply.github.com>
2025-03-23 12:50:29 +0000

    minilamized coolHelper
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ coolHelper.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name      coolHelper
+// @namespace coolHelper
+// @match     *
+// @version   0.0.1
+// @grant     none
+// ==/UserScript==
+
+
+const CoolHelpClass = class{
+    #scrollDown(){
+        window.scrollTo(0, document.body.scrollHeight);
+    }
+    #scrollUp(){
+        window.scrollTo(0, document.body.scrollHeight);
+    }
+    #scrollDownUp(){
+        setTimeout(this.#scrollDown, 100);
+        setTimeout(this.#scrollUp, 200);
+    }
+    scroll(){
+        this.#scrollDownUp();
+        setTimeout(this.#scrollDownUp, 1000);
+    }
+    iText(e, select_){
+        return  e.querySelector(select_).innerText;
+    }
+};
