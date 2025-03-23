5c9418c4131ab0fa374843daa001976aebe20103
Soldy <4786022+Soldy@users.noreply.github.com>
2025-03-23 14:29:19 +0000

    compatibility
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- coolHelper.js
+++ coolHelper.js
@@ -1,20 +1,56 @@
+/**
+ *  coolHelper is a simplified version of the old helper class
+ *  that was used in the scrapper script built in 2021. 
+ *  It's not good but make the code shorter. 
+ *
+**/
 
+/**
+ *
+ *
+**/
 const CoolHelpClass = class{
+    /**
+     * @private
+    **/
     #scrollDown(){
         window.scrollTo(0, document.body.scrollHeight);
     }
+    /**
+     * @private
+    **/
     #scrollUp(){
-        window.scrollTo(0, document.body.scrollHeight);
+        window.scrollTo(0, 0);
     }
+    /**
+     * @private
+    **/
     #scrollDownUp(){
         setTimeout(this.#scrollDown, 100);
         setTimeout(this.#scrollUp, 200);
     }
+    /**
+     * @public
+    **/
     scroll(){
         this.#scrollDownUp();
         setTimeout(this.#scrollDownUp, 1000);
     }
+    /**
+     *
+     * @param {DOMElement}
+     * @parram {strng}
+     * @public
+     * @return {string}
+    **/
     iText(e, select_){
-        return  e.querySelector(select_).innerText;
+        let d = e.querySelector(select_);
+        if(typeof d === 'undefined' || d === null){
+            d = e.getElementsByClassName(select_)[0];
+        }
+        if(typeof d === 'undefined' || d === null){
+           return '';
+        }
+        return  d.innerText;
     }
 };
