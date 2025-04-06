66423a2edd56f22daa71b4f00c401a61b507b3ce
Soldy <4786022+Soldy@users.noreply.github.com>
2025-04-06 02:32:37 +0100

    scrapper helper main class
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ scrapperHelper.js
@@ -0,0 +1,45 @@
+
+
+/**
+ * @class
+**/
+const ScrapperHelperClass = class{
+    /**
+     * @param {string} // @param {str}
+     * @constructor
+    **/
+    constructor(url_) {
+        this.cH = new CoolHelperClass();
+        this.nH = new NetHelperClass(url_);
+    }
+    /**
+     *
+     * @param {string}
+     * @param {Object.<string, string>} // @param {map<str,str>}
+     * @public
+     * @returns {Object.<string, string>} // @return {map<str,str>}
+    **/
+    searchObject(id_, data_){
+        let search = {};
+        search[id_] = data_[id_];
+        return search;
+    };
+    /**
+     *
+     * @param {DOMElement}
+     * @param {Object.<string, string | Array.<string>>} // @param {map<str, str | tuple<str, str>>}
+     * @param {string} // @param {str}
+     * @public
+    **/
+    async mineAndSave(e_, meta_, id_){
+        const mined = this.cH.iTextss(
+          e_, meta_
+        );
+        for (const i of mined){
+            await this.nH.checkAndAdd(
+              this.searchObject(id_, i),
+              i
+            );
+        }
+    }
+};
