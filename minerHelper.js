ce1bb45a388463461b06405ea9d5edd8c8d643b2
Soldy <4786022+Soldy@users.noreply.github.com>
2025-04-06 14:05:33 +0100

    clean up
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- minerHelper.js
+++ minerHelper.js
@@ -1,12 +1,43 @@
-
+/**
+ * At some times we can realize the javascript class
+ * is not always a comfortable object.
+ * Because no friend, no protected, no neighbor.
+ * That reason this class is a prototype.
+ * 
+ * The miner class is necessary only  
+ * when both scrapping and mining are required.
+ * The primary purpose of this class is to
+ * initiate a loop function that enables
+ * continuous mining with pagination.
+ *
+ *
+ * @param {Array.<DOMElement>}
+ * @param {Object.<string, string | Object<string, string>}
+ * @param {string}
+ * @param {string}
+ * @param {string}
+ * @param {?DOMELEMENT}
+ * @param {?string}
+ * @class
+**/
 const MinerHelperClass = function(element_, meta_, key_, url_, click_, text_){
+    /**
+     * Unnecessary public gate for the mine function.
+     *
+     * @public
+    **/
     this.mining = async function(){
         return mining(); 
     }
+    /**
+     * Public gate function for the loop function.
+     *
+     * @public
+    **/
     this.loop = function(){
         return paginatorLoop(); 
     }
-    const sH = new ScrapperHelperClass(url_);   
+    const sH = new ScrapperHelperClass(url_);
     const main_element = element_;
     const meta_tag = meta_;
     const search_tag = key_;
@@ -17,17 +48,25 @@ const MinerHelperClass = function(element_, meta_, key_, url_, click_, text_){
           10000 + Math.floor(Math.random() * 5000)
         );
     }
+    /**
+     *
+     * @private
+    **/
     const mining = async function(){
-        await sH.mineAndSave(main_element, meta_tag, search_tag)
+        await sH.mineAndSave(main_element, meta_tag, search_tag);
     };
+    /**
+     *
+     * @private
+    **/
     const paginatorLoop = async function(){
         setTimeout(async function(){
             await mining();
             if ( sH.cH.clickSmart(
-                paginator_click, 
+                paginator_click,
                 paginator_text
-              ) 
-            ){ 
+              )
+            ){
                 setTimeout(paginatorLoop, randWait());
             };
         }, randWait());
