ffec90c5b2df0f545b753083b78c4833414cd6f4
Soldy <4786022+Soldy@users.noreply.github.com>
2025-04-14 17:09:29 +0100

    random wait + comment
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- scrollHelper.js
+++ scrollHelper.js
@@ -1,9 +1,15 @@
+/**
+ * Scroll helper is a solution that tries to scroll down 
+ * the DOMElement again and again until the last record.
+ *
+ *
+**/
+
 
 /**
  * @param {DOMElement}
  * @param {string} //{str}
 **/
-
 const ScrollerHelperClass = function(holder_, list_element_){
     /** @type {DOMElement} **/
     const _e = holder_;
@@ -17,11 +23,24 @@ const ScrollerHelperClass = function(holder_, list_element_){
     let _sVal = 0;
     const cH = new CoolHelperClass();
     /**
+     *  Easy to detect a scrapperbot 
+     *  if you searching for periodic events.
+     *  A random number generator made it difficult. 
+     *
+     * @private
+     * @return {number}// {uint16_t}
+    **/
+    const randWait = ()=>{
+        return (
+          2000 + Math.floor(Math.random() * 2000)
+        );
+    };
+    /**
      *
      * @private
      **/
     const _again = function (){
-        setTimeout(_do, 3000);
+        setTimeout(_do, randWait());
     };
     /**
      *
@@ -30,21 +49,21 @@ const ScrollerHelperClass = function(holder_, list_element_){
     const _check = function(){
         _e.scroll(0,0);
         setTimeout(function(){
-        const length = document.querySelectorAll(_list_element).length;
-        console.debug(
-           'scroll helper check. Size : '+
-           length.toString()+' '+
-           _length.toString()
-        );
-        if( length === _length ){
+            const length = document.querySelectorAll(_list_element).length;
             console.debug(
-              'scroll helper done'
+              'scroll helper check. Size : '+
+              length.toString()+' '+
+              _length.toString()
             );
-            return _resolve(true);
-        }
-        _length = length;
-        setTimeout(_again, 3000);
-        },3000);
+            if ( length === _length ){
+                console.debug(
+                  'scroll helper done'
+                );
+                return _resolve(true);
+            }
+            _length = length;
+            setTimeout(_again, randWait());
+        },randWait());
     }
     /**
      *
@@ -52,9 +71,13 @@ const ScrollerHelperClass = function(holder_, list_element_){
      **/
     const _do = async function(){
         await cH.scroll();
-        _sVal += 10000;
+        _sVal += (
+          10000 + Math.round(
+            Math.random() * 20000
+          )
+        );
         _e.scroll(0,_sVal);
-        setTimeout(_check, 3000);
+        setTimeout(_check, randWait());
     }
     /**
      *
@@ -68,7 +91,7 @@ const ScrollerHelperClass = function(holder_, list_element_){
         const scr = this; 
         return new Promise(function (resolve, reject) {
             _resolve = resolve;
-            setTimeout(_do, 3000);
+            setTimeout(_do, randWait());
         });
     }
 }
