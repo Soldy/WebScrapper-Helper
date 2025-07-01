6234b3d8a945d683b2b3293cf6e9c8c34adf072f
Soldy <4786022+Soldy@users.noreply.github.com>
2025-07-01 15:23:09 +0100

    step helper safe step
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- stepHelper.js
+++ stepHelper.js
@@ -4,6 +4,12 @@ const stepHelperClass = class{
      * @private
      * @type {number}
     **/
+    #start = 0;
+
+    /**
+     * @private
+     * @type {number}
+    **/
     #name = 'id';
 
     /**
@@ -12,11 +18,6 @@ const stepHelperClass = class{
     **/
     #url = 'none';
 
-    /**
-     * @private
-     * @type {number}
-    **/
-    #step = 0;
 
     /**
      * @private
@@ -31,16 +32,50 @@ const stepHelperClass = class{
     #mT = '';
 
     /**
+     * @private
+    **/
+    async #step(new_id_){
+        const _url = this.#url;
+        const _new_id = new_id_;
+        await this.#sT.set(
+          this.#name,
+          _new_id
+        );
+        this.#mT.setRandOut(function(){
+             window.location = (
+               _url.replace('{id}', _new_id)
+             );
+        });
+    };
+
+    /**
      *
      * @param {string} //
      * @param {?string} //
+     * @param {?number} // {?int}
      * @constructs
     **/
-    constructor(url_, name_) {
+    constructor(url_, name_, start_) {
         this.#sT = new StorageHelperClass();
-        this.#mT = new MinerToolClass('miner', 4000, 2000);
-        if (typeof name_ === 'undefined')
+        this.#mT = new MinerToolClass('miner', 4000, 1000);
+        if (typeof name_ !== 'undefined')
             this.#name = name_.toString();
+        if (typeof start_ !== 'undefined')
+            this.#start = start_;
+        this.#url = url_;
+    };
+
+    /**
+     *
+     * @public
+     * @async
+     * @return {number} // {int}
+    **/
+    async get(){
+        const org = parseInt(await this.#sT.get(this.#name));
+        if(isNaN(org))
+             return this.#start;
+        return org;
     };
 
     /**
@@ -49,16 +84,19 @@ const stepHelperClass = class{
      * @async
     **/
     async step(){
-        const _org = parseInt(await this.#sT.get(this.#name));
-        await this.#sT.set(
-          this.#name,
-          (_org-1).toString()
-        );
-        const _url = this.#url;
-        this.#mT.setRandOut(function(){
-            window.location = (
-               _url.replace('{id}', _org.toString())
-            );
-        });
+        const _org_id = await this.get();
+        this.#step((_org_id - 1).toString());
+    };
+
+    /**
+     *
+     * @public
+     * @async
+    **/
+    async safeStep(current_id_){
+        const _org_id = await this.get();
+        if(current_id_ === _org_id)
+            return this.#step((_org_id - 1).toString());
+        this.#step(_org_id.toString());
     };
 };
